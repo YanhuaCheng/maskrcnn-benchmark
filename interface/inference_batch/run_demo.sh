8498f4581388cc36a49e724d162a403883ffbab5
lincolnlin <lincolnlin@tencent.com>
2019-07-18 11:27:08 +0800

    run_demo.sh

--- interface/inference_batch/run_demo.sh
+++ interface/inference_batch/run_demo.sh
@@ -5,6 +5,10 @@
 # File Name: train_multi_gpu.sh
 # Description: 
 #########################################################################
-CUDA_VISIBLE_DEVICES=0 python demo.py --config-file "./configs/inference_full_product11_faster_rcnn_retinanet_R-50-FPN_1x.yaml" --root-dir "../../demo/badcase_imgs" DEPLOY.TOPN 3 OUTPUT_DIR "save_result" MODEL.WEIGHT ../../pretrained_models/.torch/models/full_product_det11_448-576_retinanet_00200000.pth
-
+##retinanet
+#CUDA_VISIBLE_DEVICES=0 python demo.py --config-file "./configs/inference_full_product11_faster_rcnn_retinanet_R-50-FPN_1x.yaml" --root-dir "../../demo/badcase_imgs" DEPLOY.TOPN 3 OUTPUT_DIR "save_result" MODEL.WEIGHT ../../pretrained_models/.torch/models/full_product_det11_448-576_retinanet_00200000.pth
+##faster rcnn
 #CUDA_VISIBLE_DEVICES=0 python demo.py --config-file "./configs/inference_full_product11_faster_rcnn_R-50-FPN_1x.yaml" --root-dir "../../demo/badcase_imgs" DEPLOY.TOPN 3 OUTPUT_DIR "save_result" MODEL.WEIGHT ../../pretrained_models/.torch/models/full_product_det11_384-512_v4_classs_weight_0200000.pth
+##dconv
+CUDA_VISIBLE_DEVICES=0 python demo.py --config-file "./configs/inference_full_product11_faster_rcnn_dconv_R_50_FPN_1x.yaml" --root-dir "../../demo/badcase_imgs" DEPLOY.TOPN 3 OUTPUT_DIR "save_result" MODEL.WEIGHT ../../pretrained_models/.torch/models/full_product_det11_res50_dconv_class_weight.pth
+##gn
